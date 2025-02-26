cc91913fe17cc8d46dae847b9460ab6af633d010
alaajaraaxcode <alaa.jaraa@xcode.ae>
2025-02-27 01:34:26 +0400

    hide help

--- xcodehelp/hooks.py
+++ xcodehelp/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/xcodehelp/css/xcodehelp.css"
+app_include_css = "/assets/xcodehelp/css/xcodehelp.css"
 # app_include_js = "/assets/xcodehelp/js/xcodehelp.js"
 
 # include js, css files in header of web template
